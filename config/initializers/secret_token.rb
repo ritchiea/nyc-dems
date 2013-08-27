a847d3259f64968a3ae46fb705a7dbfa8e421861
Andrew Ritchie <ritchiea@gmail.com>
2013-08-27 18:24:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NycDems::Application.config.secret_key_base = 'ebb2db5e703816fac0f06fa14ac64dd9f0080de31e5b68fc7cfc53663c706173882ec04281f36a68ebee448f7d823aa439ff55b586629487d9e272ff1def4750'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NycDems::Application.config.session_store :cookie_store, key: '_nyc-dems_session'
