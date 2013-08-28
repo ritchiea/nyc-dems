5e9785da39fdc2df717b13bd683bfd59434dc949
Andrew Ritchie <ritchiea@gmail.com>
2013-08-28 13:37:10 -0400

    pruned more unnecessary code

--- app/assets/javascripts/app/views/map_view.js
+++ app/assets/javascripts/app/views/map_view.js
@@ -7,17 +7,14 @@ App.MapView = Ember.View.extend({
   map: null,
 
   didInsertElement: function() {
-    console.log('mapView lol')
     var pinURL = "http://chart.apis.google.com/chart?chst=d_map_pin_letter_withshadow&chld="
 
     var mapOptions = {
       center: new google.maps.LatLng(40.714, -74.0064),
-       zoom: 11,
+       zoom: 13,
        mapTypeId: google.maps.MapTypeId.ROADMAP
      }
 
-     console.log(this.$())
-     console.log(this.$().find('#map-canvas'))
     var map = new google.maps.Map(this.$().find('#map-canvas')[0], mapOptions)
   }
 
