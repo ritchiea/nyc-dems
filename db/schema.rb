a07284c5013e200cf3cc6baf6f38bd914944247d
Andrew Ritchie <ritchiea@gmail.com>
2013-08-30 03:31:57 -0400

    add candidates table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130830053651) do
+ActiveRecord::Schema.define(version: 20130830073047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20130830053651) do
     t.datetime "updated_at"
     t.string   "neighborhood"
     t.string   "county"
+  end
+
+  create_table "candidates", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "endorsements", force: true do |t|
