5e086c6c01e1eab60a8f5405b3e1e060c4470f32
Andrew Ritchie <ritchiea@gmail.com>
2013-09-04 00:34:28 -0400

    updated production environment for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ NycDems::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.initialize_on_precompile=false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
