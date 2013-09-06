12f01296e7053daaf6037aa87222238f06315645
Andrew Ritchie <ritchiea@gmail.com>
2013-09-05 21:41:43 -0400

    no comments if description is blank

--- app/models/endorsement.rb
+++ app/models/endorsement.rb
@@ -5,10 +5,16 @@ class Endorsement < ActiveRecord::Base
 
   validates :candidate_id, presence: true
 
+  before_save :check_description
   after_save :run_building_fave_candidate
 
   has_one :user
   accepts_nested_attributes_for :user, :allow_destroy => false
+
+  def check_description
+    return if !description.blank?
+    self.description = 'No comments'
+  end
 
   def run_building_fave_candidate
     building.set_favorite_candidate
